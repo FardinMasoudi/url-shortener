afcef767a1e4ce11a2756910883602f30d0d1216
fardin masoudi <70ardin@Gmail.com>
2022-05-20 23:50:35 +0430

    feat: add jwt and set middleware on routes

---
+++ app/jwt/JwtHandler.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\jwt;
+
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtHandler
+{
+    protected $jwt_secrect;
+    protected $token;
+    protected $issuedAt;
+    protected $expire;
+    protected $jwt;
+
+    public function __construct()
+    {
+        // set your default time-zone
+        date_default_timezone_set('Asia/Kolkata');
+        $this->issuedAt = time();
+
+        // Token Validity (3600 second = 1hr)
+        $this->expire = $this->issuedAt + 3600;
+
+        // Set your secret or signature
+        $this->jwt_secrect = "68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=";
+    }
+
+    public function jwtEncodeData($iss, $data)
+    {
+
+        $this->token = array(
+            //Adding the identifier to the token (who issue the token)
+            "iss" => $iss,
+
+            "aud" => $iss,
+            // Adding the current timestamp to the token, for identifying that when the token was issued.
+            "iat" => $this->issuedAt,
+            // Token expiration
+            "exp" => $this->expire,
+            // Payload
+            "data" => $data
+        );
+
+        $this->jwt = JWT::encode($this->token, $this->jwt_secrect, 'HS256');
+        return $this->jwt;
+    }
+
+    public function jwtDecodeData($jwt_token)
+    {
+        try {
+            $decode = JWT::decode($jwt_token, new Key($this->jwt_secrect, 'HS256'));
+
+            return [
+                "data" => $decode->data
+            ];
+        } catch (Exception $e) {
+            return [
+                "message" => $e->getMessage()
+            ];
+        }
+    }
+}
---
+++ app/middleware/AuthMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\middleware;
+
+use App\jwt\JwtHandler;
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
+
+class AuthMiddleware extends JwtHandler
+{
+
+    public function handle(ServerRequestInterface $request, \Closure $next)
+    {
+        if ($request->getHeader('Authorization') && $this->isTokenValid($request)) {
+            return $next($request);
+        }
+
+        return new JsonResponse(['error' => 'Unauthorized!'], 401);
+    }
+
+    public function isTokenValid($request)
+    {
+        try {
+            if (preg_match('/Bearer\s(\S+)/', $request->getHeaderLine('Authorization'), $matches)) {
+                $data = $this->jwtDecodeData($matches[1]);
+
+                if (isset($data['data']->id)) {
+                    return true;
+                } else {
+                    return [
+                        "success" => 0,
+                        "message" => $data['message'],
+                    ];
+                }
+            } else {
+                return [
+                    "success" => 0,
+                    "message" => "Token not found in request"
+                ];
+            }
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+    }
+}
