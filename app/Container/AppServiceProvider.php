c7b8d4856a56c7a7c861561118993caadeb8b131
fardin masoudi <70ardin@Gmail.com>
2022-05-31 16:13:42 +0430

    feat:add dotenv package to project

---
+++ app/Container/AppServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Container;
+
+use App\Http\Controllers\Admin\LinkController;
+
+class AppServiceProvider extends Container
+{
+    public function handle()
+    {
+//        $this->register(LinkController::class, LinkController::class);
+    }
+}
--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+// load environment
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+//load apis
 require __DIR__ . '/routes/api.php';
 
+//load redis configuration
 require __DIR__ . '/app/Redis/Link.php';
+
